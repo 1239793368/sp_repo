286a984733e7a2fa59fca61e7c2ce2bc9934066a
1239793368 <50760769+1239793368@users.noreply.github.com>
2019-06-28 09:40:34 +0800

    Update ItemServiceImpl.java
    111

--- sp02-itemservice/src/main/java/com/tedu/sp02/item/service/ItemServiceImpl.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/item/service/ItemServiceImpl.java
@@ -1,17 +1,13 @@
 package com.tedu.sp02.item.service;
-
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.tedu.sp01.pojo.Item;
 import com.tedu.sp01.service.ItemService;
-
 import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Service
 public class ItemServiceImpl implements ItemService {
-
 	@Override
 	public List<Item> getItems(String orderId) {
 		ArrayList<Item> list = new ArrayList<Item>();
@@ -22,7 +18,6 @@ public class ItemServiceImpl implements ItemService {
 		list.add(new Item(5, "商品 5",5));
 		return list;
 	}
-
 	@Override
 	public void decreaseNumbers(List<Item> list) {
 		if (log.isInfoEnabled()) {
